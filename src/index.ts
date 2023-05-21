e4d08b44abb879fce15547186c7d8b404bf2e453
blurname <naughtybao@outlook.com>
2023-05-21 09:54:06 +0800

    CHG: reduce code

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,20 @@ import graphFormatter from './formatters/graph';
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 //TODO : use import here
-const conf = require('../conf');
+// const conf = require('../conf');
 
-const { relative } = require('path');
-const { cwd } = require('process');
-const { readFileSync } = require('fs');
-const { map } = require('async');
-const { EOL } = require('os');
+// const { relative } = require('path');
+// const { cwd } = require('process');
+// const { readFileSync } = require('fs');
+// const { map } = require('async');
+// const { EOL } = require('os');
 
-const Tracer = require('pegjs-backtrace');
-const fastGlob = require('fast-glob');
+// const Tracer = require('pegjs-backtrace');
+// const fastGlob = require('fast-glob');
 
 const { parse } = require('./plantuml');
-const { parse: parseTrace } = require('./plantuml-trace');
+// const { parse: parseTrace } = require('./plantuml-trace');
+export {parse};
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 export interface ParseOptions {
@@ -30,93 +31,93 @@ export interface ParseOptions {
   verbose?: boolean;
 }
 
-function parseSync (
-  src: string,
-  options: ParseOptions = {},
-): types.UML[] {
-  options = options || {};
+// function parseSync (
+//   src: string,
+//   options: ParseOptions = {},
+// ): types.UML[] {
+//   options = options || {};
 
-  if (options.verbose) {
-    const tracer = new Tracer(src, options);
-    try {
-      const parsed = parseTrace(
-        src,
-        {
-          ...options,
-          tracer: tracer,
-        },
-      );
-      return parsed;
-    } catch (e) {
-      try {
-        e.message = 'Line ' + e.location.start.line + ': ' + e.message;
-        e.message += EOL;
-        e.message += EOL;
-        e.message += tracer.getBacktraceString();
-        e.message += EOL;
-      } catch (e) {
-        // do nothing
-      }
-      throw e;
-    }
-  }
-  return parse(src, options);
-}
+//   if (options.verbose) {
+//     const tracer = new Tracer(src, options);
+//     try {
+//       const parsed = parseTrace(
+//         src,
+//         {
+//           ...options,
+//           tracer: tracer,
+//         },
+//       );
+//       return parsed;
+//     } catch (e) {
+//       try {
+//         e.message = 'Line ' + e.location.start.line + ': ' + e.message;
+//         e.message += EOL;
+//         e.message += EOL;
+//         e.message += tracer.getBacktraceString();
+//         e.message += EOL;
+//       } catch (e) {
+//         // do nothing
+//       }
+//       throw e;
+//     }
+//   }
+//   return parse(src, options);
+// }
 
-export { parseSync as parse };
-export function parseFile (
-  globPattern: (string | string[]),
-  options: ParseOptions = {},
-  cb: (error: Error, result: types.File) => void = null,
-): types.File {
-  options = options || {};
-  // callback given
-  if (cb) {
-    return map(
-      fastGlob.sync(globPattern),
-      (
-        file: string,
-        cb: (error: Error, file: types.File) => void,
-      ) => {
-        let parseResult = null;
-        try {
-          parseResult = parseSync(
-            readFileSync(file, conf.encoding),
-            options,
-          );
-        } catch (e) {
-          return cb(e, null);
-        }
+// export { parseSync as parse };
+// export function parseFile (
+//   globPattern: (string | string[]),
+//   options: ParseOptions = {},
+//   cb: (error: Error, result: types.File) => void = null,
+// ): types.File {
+//   options = options || {};
+//   // callback given
+//   if (cb) {
+//     return map(
+//       fastGlob.sync(globPattern),
+//       (
+//         file: string,
+//         cb: (error: Error, file: types.File) => void,
+//       ) => {
+//         let parseResult = null;
+//         try {
+//           parseResult = parseSync(
+//             readFileSync(file, conf.encoding),
+//             options,
+//           );
+//         } catch (e) {
+//           return cb(e, null);
+//         }
 
-        return cb(
-          null,
-          new types.File(
-            relative(
-              cwd(),
-              file,
-            ),
-            parseResult,
-          ),
-        );
-      },
-      cb,
-    );
-  }
+//         return cb(
+//           null,
+//           new types.File(
+//             relative(
+//               cwd(),
+//               file,
+//             ),
+//             parseResult,
+//           ),
+//         );
+//       },
+//       cb,
+//     );
+//   }
 
-  // no callback given
-  return fastGlob.sync(globPattern).map(
-    (file: string) => new types.File(
-      relative(
-        cwd(),
-        file,
-      ),
-      parseSync(
-        readFileSync(file, conf.encoding),
-        options,
-      ),
-    ),
-  );
-}
+//   // no callback given
+//   return fastGlob.sync(globPattern).map(
+//     (file: string) => new types.File(
+//       relative(
+//         cwd(),
+//         file,
+//       ),
+//       parseSync(
+//         readFileSync(file, conf.encoding),
+//         options,
+//       ),
+//     ),
+//   );
+// }
 
 type Formatter = (parseResult: (types.File | types.UML[])) => any;
 type Formatters = {
